21c5e1908067dd4ef1fadaec2c90673b20d2b234
Alex Asplund <alex.asplund@assaabloy.com>
2021-09-19 13:56:43 +0200

    Edit release action

--- release.ps1
+++ release.ps1
@@ -1,3 +1,9 @@
-$guid = [guid]::NewGuid().ToString() -replace(".*-","")
-$TargetDir = "$env:temp\vanchor-$guid"
-mkdir $TargetDir
+$Version = (Cat .\version.txt ) -split "\."
+$Version[-1] = [int]$Version[-1] + 1
+$Version = $Version -join "."
+
+
+$ZipFile
+Compress-Archive -Path src\* -DestinationPath $ZipFile
+
+"$env:temp\vanchor-$version.zip"
